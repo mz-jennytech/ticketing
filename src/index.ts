1e15ff6871a93ddd7aaea355b9dec8913c963f1e
Jennifer Udensi <okosisijenifer@gmail.com>
2023-09-25 15:05:22 -0300

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-interface Color {
-  red: number;
-  blue: number;
-  green: number;
-}
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation';
 
-const color: Color = {
-  red: 20,
-  blue: 10,
-  green: 10,
-};
-
-console.log(color);
-
-export default color;
+export * from './middlewears/current-user';
+export * from './middlewears/error-handler';
+export * from './middlewears/require-auth';
+export * from './middlewears/validate-request';
