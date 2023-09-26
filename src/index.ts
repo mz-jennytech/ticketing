a76c7fe3ce691e9cf545d7d3db55ca30891b476d
Jennifer Udensi <okosisijenifer@gmail.com>
2023-09-25 21:34:17 -0300

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// Re-export stuff from errors and middlewears
 export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
