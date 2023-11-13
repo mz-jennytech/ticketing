a9c03e25bb33db2ed06fb858ae5579aaa0386935
Jennifer Udensi <okosisijenifer@gmail.com>
2023-11-13 19:48:11 -0300

    fixed an error

--- client/pages/orders/index.js
+++ client/pages/orders/index.js
@@ -12,7 +12,7 @@ const OrderIndex = ({ orders }) => {
   );
 };
 
-OrderIndex.getInitaialProps = async (context, client) => {
+OrderIndex.getInitialProps = async (context, client) => {
   const { data } = await client.get('/api/orders');
 
   return { orders: data };
